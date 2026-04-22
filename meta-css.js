a7ad0c19354dd785b84f7e36d3d34bf2b32ae7e9
f3lipeL4w <felipelawlds@gmail.com>
2026-04-22 17:47:22 -0600

    wdd131

---
+++ meta-css.js
@@ -0,0 +1,39 @@
+// variables vs constant
+
+const PI = 3.14;
+let radius = 3;
+
+let area = radius * radius * PI;
+
+console.log(area); // 28.26
+
+radius = 20;
+area = radius * radius * PI;
+console.log(area); // 1256
+
+radius = 50;
+area = radius * radius * PI;
+console.log(area);
+
+// type coersion
+
+const one = 1;
+const two = '2';
+
+let result = one * two;
+console.log(result);
+result = one + Number(two);
+console.log(result);
+
+// scope
+
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
